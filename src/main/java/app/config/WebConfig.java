21e6d7c4880aee1649135ef15085862a9dd9d9ad
ivan mamani <ivanric12@gmail.com>
2025-02-20 14:22:02 -0400

    modificacion de evento v-1.1.3

--- src/main/java/app/config/WebConfig.java
+++ src/main/java/app/config/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Permite todas las rutas
-            .allowedOrigins("*")  // Permite solicitudes desde cualquier origen
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")  // Permite todos los métodos
-            .allowedHeaders("*")  // Permite todos los encabezados
-            .allowCredentials(false)  // Si no deseas permitir credenciales (cookies, autenticación HTTP)
-            .maxAge(3600);  // Cache de la configuración CORS durante 1 hora (en segundos)
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")  // Permite todas las rutas
+//            .allowedOrigins("*")  // Permite solicitudes desde cualquier origen
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")  // Permite todos los métodos
+//            .allowedHeaders("*")  // Permite todos los encabezados
+//            .allowCredentials(false)  // Si no deseas permitir credenciales (cookies, autenticación HTTP)
+//            .maxAge(3600);  // Cache de la configuración CORS durante 1 hora (en segundos)
+//    }
 }
